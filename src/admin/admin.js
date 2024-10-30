17e460a6429a7ada758ada3fefd3536e8a8f4e64
Masa <masa.qazmoz@gmail.com>
2024-10-30 22:49:00 +0200

    no message

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const adminController = require('./admin.controller');
-const verifyAdmin = require('../middleware'); // Middleware for authentication
+const { authenticateJWT } = require('../middleware/middleware.js'); 
 
 const router = express.Router();
 
 // Routes for admin management
-router.post('/dresses', verifyAdmin, adminController.addDress);
-router.delete('/dresses/:id', verifyAdmin, adminController.deleteDress);
-router.put('/dresses/:id', verifyAdmin, adminController.updateDress);
+router.post('/dresses', athenticateJWT, adminController.addDress);
+router.delete('/dresses/:id', authenticateJWT, adminController.deleteDress);
+router.put('/dresses/:id', authenticateJWT, adminController.updateDress);
 module.exports = router;
