f2b65caf9479d739156c10fcfc406f0cb8c0f9a8
Masa <masa.qazmoz@gmail.com>
2024-11-01 21:03:03 +0200

    no message

--- db/connection.js
+++ db/connection.js
@@ -11,7 +11,6 @@ const sequelize = new Sequelize(
     }
 );
 
-// Test the connection
 sequelize.authenticate()
     .then(() => console.log('Database connected...'))
     .catch(err => console.log('Error: ' + err));
