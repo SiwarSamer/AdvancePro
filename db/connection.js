aea52d76c71f99ceeedbb3164ae5eafc903efb29
Masa <masa.qazmoz@gmail.com>
2024-11-01 21:00:50 +0200

    no message

--- db/connection.js
+++ db/connection.js
@@ -7,13 +7,29 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'mysql'
+        dialect: 'mysql',
     }
 );
 
+// Test the connection
 sequelize.authenticate()
     .then(() => console.log('Database connected...'))
     .catch(err => console.log('Error: ' + err));
 
-module.exports = sequelize;
+// Import models
+const User = require('./user.model');
+const Rental = require('./rental.model');
+const Review = require('./review.model');
+const Recommendation = require('./recommendation.model');
 
+// Define associations
+User.hasMany(Review, { foreignKey: 'user_id' });
+Rental.hasMany(Review, { foreignKey: 'rental_id' });
+Review.belongsTo(User, { foreignKey: 'user_id' });
+Review.belongsTo(Rental, { foreignKey: 'rental_id' });
+
+User.hasMany(Recommendation, { foreignKey: 'user_id' });
+Recommendation.belongsTo(User, { foreignKey: 'user_id' });
+
+// Export the sequelize instance and the models
+module.exports = { sequelize, User, Rental, Review, Recommendation };
