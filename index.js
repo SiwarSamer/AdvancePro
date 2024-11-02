9919d6e10b7d6189ffe05b411270a9860ee4d8a8
DESKTOP-G7MHP6V\hp <masa.nabeel1@gmail.com>
2024-11-02 02:41:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+// Import necessary modules
 const express = require('express');
 const sequelize = require('./db/connection.js');
 const userRoutes = require('./src/modules/roles/users/user.js');
 const authRoutes = require('./src/modules/auth/auth.js'); 
-const app = express();
 const cors = require('cors');
-const { Dress, Wishlist } = require('./db/models/associations'); 
 require('dotenv').config();
 
+const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
+// Route handlers
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes); 
 
+// Sync the database and start the server
 sequelize.sync()
-    .then(() => console.log('Database synced'))
-    .catch(err => console.log(err));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
+    .then(() => {
+        console.log('Database synced');
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch(err => {
+        console.error('Database sync failed:', err);
+        process.exit(1); // Exit process with failure
+    });
